7b6f80c67718ee54b23b7360ac1258ac75978848
Matthew Haddock <777mth@gmail.com>
2017-12-21 12:04:18 -0500

    improve scenes and add timing

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,12 +2,63 @@
 
   var usedQuestions = [];
   var questionLimit = 10;
-  var qIndex = [];
+  var qIndex;
   var currentQuestion;
+  var questionsAsked;
+  var questionsCorrect;
+  var intervalId;
+  var timeLimit;
+
+  function answerDecrement() {
+    timeLimit--;
+    // console.log("timeLimit", timeLimit);
+    $("#timeRemaining").text("Time remaining: " + timeLimit + " seconds!");
+    if (timeLimit === 0) {
+      answerTimeUp();
+    }
+  };
+
+  function answerTimer() {
+    intervalId = setInterval(answerDecrement, 1000);
+    console.log("answerTimer() executed");
+  };
+  
+  function answerTimeUp() {
+    console.log("executing answerTimeUp()");
+    clearInterval(intervalId);
+    wasAnswered = "no";
+    endOfQuestion();
+  };
+
+  function roundDecrement() {
+    timeLimit--;
+    // console.log("timeLimit", timeLimit);
+    $("#timeRemaining").text("Next question in " + timeLimit + " seconds!");
+    if (timeLimit === 0) {
+      roundTimeUp();
+    }
+  };
+
+  function roundTimer() {
+    intervalId = setInterval(roundDecrement, 1000);
+    console.log("roundTimer() executed");
+  };
+  
+  function roundTimeUp() {
+    console.log("executing roundTimeUp()");
+    clearInterval(intervalId);
+    playRound();
+  };
+
+  // function pickAnswer() {
+  //   clearInterval(intervalId);
+  // }
+
 
     
   function refreshDisplay(currentQuestion) {
-    $("#currentQuestion").text(currentQuestion.question + "  " + questions.indexOf(currentQuestion));
+    console.log("refreshDisplay() executing");
+    $("#currentQuestion").text(currentQuestion.question);
     $("#questionCount").text(questionsCorrect + " / " + questionsAsked);
     $("#choice1 p").text(currentQuestion.choice1);
     $("#choice2 p").text(currentQuestion.choice2);
@@ -15,62 +66,102 @@
     $("#choice4 p").text(currentQuestion.choice4);
   };
 
-  function checkVars(location) {
-    console.log(location, "  usedQuestions: ", usedQuestions);
-    console.log(location, "  currentQuestion: ", currentQuestion);
-    console.log("");
-  };
+  // function checkVars(location) {
+    // console.log(location, "  usedQuestions: ", usedQuestions);
+    // console.log(location, "  currentQuestion: ", currentQuestion);
+    // console.log("");
+  // };
 
   function pickQuestion() {
     qIndex = (Math.floor(Math.random() * questions.length));
-    console.log("inside pickQuestion(), before IF, qIndex: ", qIndex);
+    // console.log("inside pickQuestion(), before IF, qIndex: ", qIndex);
     if (usedQuestions.indexOf(qIndex) === -1) {
       usedQuestions.push(qIndex);
-      console.log("inside pickQuestion(), after IF (qIndex not in usedQuestions[]), qIndex: ", qIndex)
+      console.log("adding new qIndex to usedQuestions[]")
       return qIndex;
     } 
     else {
-      console.log("inside pickQuestion(), after ELSE (qindex IS in usedQuestions[]), qIndex: ", qIndex);
+      console.log("rejecting new qIndex, executing pickQuestion() to find another");
       pickQuestion();
     };
-    checkVars("end of pickQuestion()");
-    console.log("end of pickQuestion() qIndex: ", qIndex);
+    // checkVars("end of pickQuestion()");
+    // console.log("end of pickQuestion() qIndex: ", qIndex);
     return qIndex;
   };
   
   
-  
-  
-  function startGame() {
+  function newGameScreen() {
+    console.log("newGameScreen() executing");
     $("#newGame").toggleClass("hidden");
-    usedQuestions = [];
-    
-    function newQuestion() {
-
-      // function checkChoice() {
-      //   if $(this.id)=""
-      // }
-      console.log("-----MOUSE CLICK-----");
-      if (questionsAsked < 8) {
-        currentQuestion = questions[pickQuestion()];
-        console.log("in newQuestion() currentQuestion: ", currentQuestion);
-        questionsAsked++;
-        refreshDisplay(currentQuestion);
-        console.log("in playRound() usedQuestions: ", usedQuestions);
-        // $(".choice").on("click", checkChoice);
-      }
-      else {
-        $("body").off("click");
-      }
+    $("#choiceArea").toggleClass("hidden");
+    $("#flavorImage").toggleClass("hidden");
+    $("#currentQuestion").toggleClass("hidden");
+    $("#numbers").toggleClass("hidden");
+  }
+  
+  function askQuestion() {
+    console.log("askQuestion() executing");
+    currentQuestion = questions[pickQuestion()];
+    refreshDisplay(currentQuestion);
+    questionsAsked++;
+  };
+  
+  function pickAnswer() {
+    console.log("pickAnswer() executing, this: ", this);
+    // console.log("this.id: ", this.id);
+    // console.log("qIndex: ", qIndex);
+    // console.log('questions', questions)
+    // console.log("questions[qIndex].answer: ", questions[qIndex].answer);
+    if (this.id === questions[qIndex].answer) {
+      wasAnswered = "correct";
+    } else {
+      wasAnswered = "incorrect";
     };
-    
-    $("body").on("click", newQuestion);
-    
+    endOfQuestion();
   };
 
-  var questionsAsked = 0;
-  var questionsCorrect = 0;
-  
-  startGame();
+  function endOfQuestion() {
+    console.log("endOfQuestion() executing");
+    if (wasAnswered === "no") {
+      console.log("Didn't answer in time!");
+    } else if (wasAnswered === "correct") {
+      console.log("Correct answer!");
+    } else {
+      console.log("Bad answer!");
+    }
+    clearInterval(intervalId);
+    timeLimit = 10;
+    roundTimer();
+  };
+
+  function playRound() {
+    console.log("playRound() executing");
+    if (questionsAsked < 10) {
+      console.log("questionsAsked: ", questionsAsked);
+      var wasAnswered = "";
+      var wasCorrect = "";
+      askQuestion();
+      timeLimit = 7;
+      answerTimer();
+      $(".choice").on("click", pickAnswer);
+    }
+  };
+
+  function startGame() {
+    console.log("startGame() executing");
+    newGameScreen();
+    $("body").off("click", startGame);
+    questionsCorrect = 0;
+    usedQuestions = [];
+    questionsAsked = 0;
+    playRound();
+  };
+
+  $("body").on("click", startGame);
 
 // }); // end ready() on document load
+
+// click to start, start timer
+// when either time runs out or question is selected
+//  reset timer (not the interval), and load next question
+// repeat until you have no more questions (use ifs to check)
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -6,8 +6,8 @@ var questions = [
     choice2 : "Kane Hodder",
     choice3 : "Gunnar Hansen",
     choice4 : "Jason Momoa",
-    answer : choice2,
-    funFact : "In addition to his work as Jason in four Ft13 movies, Kane also did stunt work as Leatherface in 'The Texas Chainsaw Massacre III', and played Freddy Krueger's gloved hand at the end of 'Jason Goes To Hell: The Final Friday'.",
+    answer : "choice2",
+    funFact : "In addition to his work as Jason in four Ft13 movies, Kane also did stunts as Leatherface in 'The Texas Chainsaw Massacre III', and played Freddy Krueger's gloved hand at the end of 'Jason Goes To Hell: The Final Friday'.",
     pic1 : "",
     pic2 : ""
   },
@@ -15,10 +15,10 @@ var questions = [
   {
     question : "What was the first horror film to be nominated for an Academy Award for Best Picture?",
     choice1 : "Jaws",
-    choice2 : "The Texas Chainsaw Massacre",
+    choice2 : "Psycho",
     choice3 : "The Omen",
     choice4 : "The Exorcist",
-    answer : choice4,
+    answer : "choice4",
     funFact : "'The Exorcist' earned 10 Oscar nominations in 1975, including a Best Supporting Acress nod for 15-year-old Linda Blair.",
     pic1 : "",
     pic2 : ""
@@ -30,7 +30,7 @@ var questions = [
     choice2 : "Jack the Ripper",
     choice3 : "Ed Gein",
     choice4 : "Son of Sam",
-    answer : choice3,
+    answer : "choice3",
     funFact : "Although Ed Gein decorated his farmhouse in body parts, he only admitted to two murders, and a whole lot of graverobbing.",
     pic1 : "",
     pic2 : ""
@@ -42,7 +42,7 @@ var questions = [
     choice2 : "'The Howling's onscreen werewolf transformation",
     choice3 : "producing the first practical blood squib",
     choice4 : "pioneering stop-motion animation",
-    answer : choice4,
+    answer : "choice4",
     funFact : "The 2001 animated film 'Monsters, Inc.' pays tribute by including a sushi restaurant called Harryhausen's.",
     pic1 : "",
     pic2 : ""
@@ -54,7 +54,7 @@ var questions = [
     choice2 : "Elizabeth Bathory",
     choice3 : "Tomas de Torquemada",
     choice4 : "Vlad Tepes",
-    answer : choice4,
+    answer : "choice4",
     funFact : "Vlad (the Impaler) Tepes was ruler of Wallachia after his father, and used brutal tactics in his struggle against the Ottoman Empire. After his father's induction into the Order of the Dragon, the family gained the honorary surname Dracul.",
     pic1 : "",
     pic2 : ""
@@ -66,7 +66,7 @@ var questions = [
     choice2 : "The Shining",
     choice3 : "Carrie",
     choice4 : "Firestarter",
-    answer : choice1,
+    answer : "choice1",
     funFact : "To this day, Stephen King is quite vocal about his dislike for 'The Shining', which is commonly regarded as one of the greatest movies ever made.",
     pic1 : "",
     pic2 : ""
@@ -78,7 +78,7 @@ var questions = [
     choice2 : "rosso",
     choice3 : "giallo",
     choice4 : "poliziotteschi",
-    answer : choice3,
+    answer : "choice3",
     funFact : "The word 'giallo' is Italian for 'yellow'. The term derives from a series of cheap paperback mystery novels with yellow covers that were popular in post-fascist Italy.",
     pic1 : "",
     pic2 : ""
@@ -87,15 +87,100 @@ var questions = [
   {
     question : "Actors Lance Henriksen and Bill Paxton both share the honor of...",
     choice1 : "receiving Oscar nominations for playing vampires",
-    choice2 : "being the only actors killed on screen by a Predator, a Terminator, and an Alien",
+    choice2 : "being killed on screen by a Predator, a Terminator, and an Alien",
     choice3 : "being related to each other as cousins",
     choice4 : "being passed up for the role of Freddy Krueger",
-    answer : choice2,
+    answer : "choice2",
     funFact : "Bill Paxton was the sole actor with this distinction until the filming of 'Alien vs. Predator' in 2004.",
     pic1 : "",
     pic2 : ""
   }, 
+
+  {
+    question : "In the original 'Psycho', interior scenes of the Bates house were shot on the old sets of what other classic movie?",
+    choice1 : "'Rear Window'",
+    choice2 : "'The Phantom of the Opera'",
+    choice3 : "'The Pit and the Pendulum'",
+    choice4 : "'Frankenstein'",
+    answer : "choice2",
+    funFact : "The Bates house did not actually exist; only two sides of the exterior are visible in the movie, and they were false facades.",
+    pic1 : "",
+    pic2 : ""
+  },
+
+  {
+    question : "How long did director John Carpenter spend writing the original script for 'Halloween'?",
+    choice1 : "10 days",
+    choice2 : "1 year",
+    choice3 : "10 years",
+    choice4 : "He didn't write it",
+    answer : "choice1",
+    funFact : "Carpenter named many of the film's characters after friends and influences, like Dr. Sam Loomis, whose namesake was a character in 'Psycho'. (The latter Loomis's girlfriend Marion, famously killed in the shower, was played by the mother of Jamie Lee Curtis, the star of 'Halloween'.) ",
+    pic1 : "",
+    pic2 : ""
+  },
+
+  {
+    question : "In addition to directing, John Carpenter is known for contributing what else to many of his films?",
+    choice1 : "Poster art",
+    choice2 : "Members of his family as actors",
+    choice3 : "Special effects",
+    choice4 : "Music",
+    answer : "choice4",
+    funFact : "Carpenter completed the musical score for 'Halloween' by himself in only three days.",
+    pic1 : "",
+    pic2 : ""
+  },
+
+  {
+    question : "Acclaimed director David Cronenberg and legendary writer Clive Barker have worked together on exactly one movie. What was it?",
+    choice1 : "'Hellraiser'",
+    choice2 : "'Videodrome'",
+    choice3 : "'eXistenZ'",
+    choice4 : "'Nightbreed'",
+    answer : "choice4",
+    funFact : "Cronenberg's own film 'Videodrome' is paid tribute to at the end of 'Nightbreed', when his character is revived by a hand thrust into his stomach.",
+    pic1 : "",
+    pic2 : ""
+  },
+
+  {
+    question : "What material was used for 'The Exorcist's possessed Regan's vomit?",
+    choice1 : "melted mint ice cream",
+    choice2 : "a mixture of water-based paint and gelatin",
+    choice3 : "watered down Play-Doh",
+    choice4 : "split pea soup",
+    answer : "choice4",
+    funFact : "",
+    pic1 : "",
+    pic2 : ""
+  }
+
+  // {
+  //   question : "",
+  //   choice1 : "",
+  //   choice2 : "",
+  //   choice3 : "",
+  //   choice4 : "",
+  //   answer : "",
+  //   funFact : "During production, the refrigerator containing the animal guts was left unplugged over the weekend, and the zombie cast was forced to act with (and simulate eating) rotting meat.",
+  //   pic1 : "",
+  //   pic2 : ""
+  // },
+
+  // {
+  //   question : "",
+  //   choice1 : "",
+  //   choice2 : "",
+  //   choice3 : "",
+  //   choice4 : "",
+  //   answer : "",
+  //   funFact : "",
+  //   pic1 : "",
+  //   pic2 : ""
+  // }
 ];
+
 
 // var blankQuestion = [
 
